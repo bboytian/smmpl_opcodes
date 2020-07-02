fe64301a0507494cbe888324c59d036a9b68c8ea
bboytian <94tian@gmail.com>
2020-07-02 15:45:47 +0800

    sop.file_man.mpl2solaris_datasync/trying out subprocess to run rsync, test file created edit 6

--- test.py
+++ test.py
@@ -18,9 +18,7 @@ syncday_lst = [
 cmd_l = [
     f'{dc_gfunc(WINDOWFILESDIR, RSYNCFILE)}',
     '-azzvi',
-    f"-e '{dc_gfunc(WINDOWFILESDIR, SSHFILE)}'",
-    "-o 'StrictHostKeyChecking=no'",
-    "-i '/cygdrive/c/Users/mpluser/.ssh/id_rsa'",
+    f"-e '{dc_gfunc(WINDOWFILESDIR, SSHFILE)}' -o 'StrictHostKeyChecking=no' -i 'C:/Users/mpluser/.ssh/id_rsa'",
     '/cygdrive/c/Users/mpluser/Desktop/smmpl_opcodes/test2020002test.txt',
     '{}@{}:{}'.format(SOLARISUSER, SOLARISIP, '/home/tianli/Desktop')
 ]
