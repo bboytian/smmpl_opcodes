df4e4acd947f742e98fc7e044d729244fc191477
bboytian <94tian@gmail.com>
2020-07-15 17:32:03 +0800

    skyscan_main.py logging debug

--- skyscan_main.py
+++ skyscan_main.py
@@ -117,6 +117,7 @@ def main(
         ## sigmampl_boot
         SETLOGFN(sigmamplboot_logdir)
         sop.sigmampl_boot(coldstart_boo=True)
+        print('where did the log go?')
         SETLOGFN(main_logdir)  # giving log control back to main log
 
         print(f'letting SigmaMPL warm up for {SIGMAMPLWARMUP}s before continuing with usual operations')
