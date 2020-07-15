42c8148f5874efb7e300618358399b06d76dded2
bboytian <94tian@gmail.com>
2020-07-15 18:03:14 +0800

    skyscan_main.py logging debug 0.3

--- functions.py
+++ functions.py
@@ -50,7 +50,9 @@ def DIRCONFN(*dirl):
 
 def SETLOGFN(stdoutlog=None, stderrlog=None):
     if stdoutlog:               # setting new logfile
+        print(sys.stdout, sys.stderr)
         SETLOGFN()
+        print(sys.stdout, sys.stderr)
         if stdoutlog != '<stdout>':
             sys.stdout = open(stdoutlog, 'a+')
         if stderrlog != '<stderr>':
@@ -58,6 +60,7 @@ def SETLOGFN(stdoutlog=None, stderrlog=None):
                 sys.stderr = open(stderrlog, 'a+')
             else:
                 sys.stderr = open(stdoutlog, 'a+')
+        print(sys.stdout, sys.stderr)
     else:           # resets the stdout and stderr to go sys default
         if sys.stdout.name != '<stdout>':
             sys.stdout.close()
--- skyscan_main.py
+++ skyscan_main.py
@@ -117,9 +117,6 @@ def main(
         ## sigmampl_boot
         SETLOGFN(sigmamplboot_logdir)
         sop.sigmampl_boot(coldstart_boo=True)
-        print(main_logdir)
-        import sys
-        print(sys.stdout.name, sys.stderr.name)
         SETLOGFN(main_logdir)  # giving stdout back to main log
 
         print(f'letting SigmaMPL warm up for {SIGMAMPLWARMUP}s before continuing with usual operations')
