1405d8e8a71b58dffdf051238236dd9b7ff5deab
bboytian <94tian@gmail.com>
2020-07-15 20:31:50 +0800

    globalimports/ made into a package with internal non conflicting importing structure 1

--- globalimports/functions.py
+++ globalimports/functions.py
@@ -1,8 +1,25 @@
 # imports
 import sys
 
-from ._functionwrappers import *
 from ._modifications import *
+
+
+# defining decorators for global functions
+def haltlogging(func):
+    '''
+    Halts redirection of stdout and stderr logfiles and outputs to __stdout__ and
+    __stderr__
+    '''
+    @wraps(func)
+    def wrapper_func(*args, **kwargs):
+        oldstdout_logdir = sys.stdout.name
+        oldstderr_logdir = sys.stderr.name
+        SETLOGFN()
+        ret = func(*args, **kwargs)
+        SETLOGFN(oldstdout_logdir, oldstderr_logdir)
+        return ret
+    return wrapper_func
+
 
 # defining functions
 
