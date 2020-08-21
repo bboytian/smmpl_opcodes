020351bd06f017db0914cf4e8602e9281ce018f6
bboytian <94tian@gmail.com>
2020-08-21 15:28:00 +0800

    bugfix 0

--- __main__.py
+++ __main__.py
@@ -5,53 +5,42 @@ from .scan_event import main as scan_event
 
 
 # handles signals
-def _handlerhook_f(dwCtrlType, hook_sigint=_thread.interrupt_main):
-    if dwCtrlType == 0:         # CTRL_C_EVENT
-        print('sending out interrupt')
-        hook_sigint()
-        return 1                # don't chain to the next handler
-    else:
-        return 0                # chain to the next handler
-
 def _handler_f(signalnum, frame):
     '''called when signal.signal interrupt is sent'''
-    try:
-        print(frame, frame.f_back)
-        framename, framefile = FRAMEPARSEFN(frame)
-        parframename, parframefile = FRAMEPARSEFN(frame.f_back)  # parent frame
-        print(framename, framefile)
-        print(parframename, parframefile)
+    print(frame, frame.f_back)
+    framename, framefile = FRAMEPARSEFN(frame)
+    parframename, parframefile = FRAMEPARSEFN(frame.f_back)  # parent frame
+    print(framename, framefile)
+    print(parframename, parframefile)
 
-        # main thread func
-        if framename == 'main' and parframename == 'module':
-            # graceful closure are handled by the measurement protocols
-            pass
-
-        # functions within main thread func
-        elif framename == 'main' and parframename == 'main':
-            raise mains.MeasurementInterrupt
-
-        # processes started in main thread func
-        elif parframefile == 'run' and DIRPARSEFN(parframefile, -2) == 'scan_event':
-            raise mains.ScaneventInterrupt
-
-        # for consecutive interrupts
-        elif framename == 'poll' and parframename == 'wait':
-            haltlogging(lambda: print('graceful program closure in progress...'))()
-
-        # for all other grandchild processes
-        elif parframename == 'run':
-            pass
-
-        else:
-            haltlogging(lambda: print(
-                'unknown frame:'
-                f'\t frame: {str(frame)}'
-                f'\t parent frame: {str(frame._fback)}'
-            ))()
-    except AttributeError:
-        print('maybe this is the cause')
+    # main thread func
+    if framename == 'main' and parframename == 'module':
+        # graceful closure are handled by the measurement protocols
         pass
+
+    # functions within main thread func
+    elif framename == 'main' and parframename == 'main':
+        raise mains.MeasurementInterrupt
+
+    # processes started in main thread func
+    elif parframefile == 'run' and DIRPARSEFN(parframefile, -2) == 'scan_event':
+        raise mains.ScaneventInterrupt
+
+    # for consecutive interrupts
+    elif framename == 'poll' and parframename == 'wait':
+        haltlogging(lambda: print('graceful program closure in progress...'))()
+
+    # for all other grandchild processes
+    elif parframename == 'run':
+        pass
+
+    else:
+        haltlogging(lambda: print(
+            'unknown frame:'
+            f'\t frame: {str(frame)}'
+            f'\t parent frame: {str(frame._fback)}'
+        ))()
+
 
 # main func
 def main(normalopsboo):
